4115908527e1b80ed7c5941a0a2297bcd1f61e3e
Bakhtiar_Aleem_Victus <bakhtiar.aleem93@gmail.com>
2025-06-15 18:28:18 +0500

    color fixes

--- assets/custom.js
+++ assets/custom.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             overlay.style.setProperty('--color', color);
             const colorSet = (color) => {
-                 if(color === '#FFF544'){
+            if(color === '#000'){
                 btn.style.color = '#fff';
             }
             else{
